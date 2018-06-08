3914367eaf6da31a4091dffa86b5afb7aba9b6ac
Thiago Sanches <thiagom@ciandt.com>
2018-06-08 13:24:36 -0300

    Adding dockerfile

--- Translator.WebApi/Controllers/ValuesController.cs
+++ Translator.WebApi/Controllers/ValuesController.cs
@@ -3,42 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Translator.Model;
+using Translator.Core;
 
 namespace Translator.WebApi.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public string Post([FromBody]PackageDefinition packageDefinition)
         {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            try
+            {
+                Resolver resolver = new Resolver();
+                return resolver.Translate(packageDefinition);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
