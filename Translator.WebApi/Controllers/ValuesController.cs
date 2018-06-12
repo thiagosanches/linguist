a169ffcedb5a8da85b76bef6b29165bf473eb9af
Thiago <tsigwt@gmail.com>
2018-06-12 08:53:33 -0300

    Update ValuesController.cs

--- Translator.WebApi/Controllers/ValuesController.cs
+++ Translator.WebApi/Controllers/ValuesController.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web.Http;
 using Microsoft.AspNetCore.Mvc;
 using Translator.Model;
 using Translator.Core;
