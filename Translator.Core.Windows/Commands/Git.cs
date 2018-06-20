2b4aeb2a6ff3aa53ad968ad61ab2408e9305f154
Thiago Sanches <thiagom@ciandt.com>
2018-06-20 20:02:49 -0300

    Updating the Git command from Windows to handle the git alias inside of powershell script to avoid touching in the PATH environment.

--- Translator.Core.Windows/Commands/Git.cs
+++ Translator.Core.Windows/Commands/Git.cs
@@ -9,8 +9,9 @@ namespace Translator.Core.Windows
     public class Git : ICommand
     {
         private const string GIT_CLONE_STATEMENT = "git clone {0}";
-        private const string GIT_CREDENTIAL_HELPER_STATEMENT_ON = "git config credential.helper cache";
-        private const string GIT_CREDENTIAL_HELPER_STATEMENT_OFF = "git credential-cache exit";
+        private const string GIT_CREDENTIAL_HELPER_STATEMENT_ON = "git config --global credential.helper store";
+        private const string GIT_CREDENTIAL_HELPER_STATEMENT_OFF = "git config --global --unset credential.helper; rm ~/.git-credentials";
+        private const string GIT_ALIAS = @"Set-Alias git $env:programfiles\Git\bin\git.exe";
         public string CreateStatement(string repository)
         {
             StringBuilder statement = new StringBuilder();
@@ -26,6 +27,7 @@ namespace Translator.Core.Windows
         public string CreateStatements(Recipe recipe)
         {
             StringBuilder statements = new StringBuilder();
+            statements.AppendLine(GIT_ALIAS);
             statements.AppendLine(GIT_CREDENTIAL_HELPER_STATEMENT_ON);
 
             foreach (var repository in recipe.GitRepositories)
